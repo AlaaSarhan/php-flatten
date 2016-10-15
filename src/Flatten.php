7f50476084ba006394fc1acb8bbc16663794bbb6
Alaa Sarhan <sarhan.alaa@gmail.com>
2016-10-15 12:46:26 +0000

    Added support for FLAG_NUMERIC_NOT_FLATTENED flag. Closes #1.

--- src/Flatten.php
+++ src/Flatten.php
@@ -8,45 +8,69 @@ namespace Sarhan;
 class Flatten
 {
     /**
-     * Flattens a variable, possible traversable, into a one-dimensional array, recursively.
+     * Turn off flattening values with integer keys.
+     */
+    const FLAG_NUMERIC_NOT_FLATTENED = 0b1;
+    
+    /**
+     * Flattens a variable, possibly traversable, into a one-dimensional array, recursively.
      * 
-     * Each key in the returned one-dimensional array is the join of all keys leading to each value, in all dimensions,
-     * separated by a given separator. That is a fully-qualified key.
+     * Each key (fully-qualified key or FQK) in the returned one-dimensional array is the join of all keys leading to
+     * each (non-traversable) value, in all dimensions, separated by a given separator.
      * 
-     * Non-traversable values will be returned as-is, after being put into the final array with the fully-qualified key.
-     * 
-     * An initial prefix can be optionally provided, but it will not separated using the separator.
+     * An initial prefix can be optionally provided, but it will not be separated with the specified separator.
      * 
      * @param mixed $var
      * @param string $separator
      * @param string $prefix
+     * @param int $flags
      * @return array One-dimensional array containing all values from all possible traversable dimensions in given input.
+     * @see Flatten::FLAG_NUMERIC_NOT_FLATTENED
      */
-    public static function flatten($var, $separator = '.', $prefix = '')
+    public static function flatten($var, $separator = '.', $prefix = '', $flags = 0)
     {
         $flattened = [];
-        foreach (self::flattenGenerator($var, $separator, '') as $key => $value) {
+        foreach (self::flattenGenerator($var, $separator, '', $flags) as $key => $value) {
             $flattened[$prefix . $key] = $value;
         }
         return $flattened;
     }
     
-    private static function flattenGenerator($var, $separator, $prefix = '')
+    private static function flattenGenerator($var, $separator, $prefix = '', $flags)
     {
-        if (self::canTraverse($var)) {
-            $prefix .= (empty($prefix) ? '' : $separator);
-            foreach ($var as $key => $value) {
-                foreach (self::flattenGenerator($value, $separator, $prefix .$key) as $k => $v) {
-                    yield $k => $v;
-                }
-            }
-        } else {
+        if (!self::canTraverse($var)) {
             yield $prefix => $var;
+            return;
+        }
+        
+        if ($flags & self::FLAG_NUMERIC_NOT_FLATTENED) {
+            list ($values, $var) = self::filterNumericKeysAndGetValues($var);
+            yield $prefix => $values;
+        }
+        
+        $prefix .= (empty($prefix) ? '' : $separator);
+        foreach ($var as $key => $value) {
+            foreach (self::flattenGenerator($value, $separator, $prefix . $key, $flags) as $k => $v) {
+                yield $k => $v;
+            }
         }
     }
     
     private static function canTraverse($var)
     {
         return is_array($var) || ($var instanceof \Traversable);
+    }
+    
+    private static function filterNumericKeysAndGetValues($var)
+    {
+        $values = [];
+        $var = array_filter($var, function($value, $key) use (&$values) {
+            if (is_int($key)) {
+                $values[$key] = $value;
+                return false;
+            }
+            return true;
+        }, ARRAY_FILTER_USE_BOTH);
+        return [$values, $var];
     }
 }
